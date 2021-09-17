aad5dcbed80f02879b7c34ba0ec6882f93e3e5f7
HAYASAKARyosuke <hayasaka.ryosuke@gmail.com>
2021-09-17 21:56:13 +0900

    change all unsafe

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,32 @@ use wio::entry;
 
 #[entry]
 fn main() -> ! {
-    unsafe {
         const PA_OUTSET: u32 = 0x41008018;
         const PA_OUTCLR: u32 = 0x41008014;
 
         const PA_DIRSET: u32 = 0x41008008;
 
-        *(PA_DIRSET as *mut u32) = 1 << 15; // PA15ピンを出力ポートに設定
+        unsafe {
+            *(PA_DIRSET as *mut u32) = 1 << 15; // PA15ピンを出力ポートに設定
+        }
 
         // PCグループのピンの入力をあつかうレジスタを選択
         const PC_IN: u32 = 0x41008020 + 0x80 * 2; // PCグループ(0x80 * 2)
 
         const PC_PINCFG: u32 = 0x41008040 + (0x80 * 2) + (0x01 * 26); // PCグループ(0x80 * 2), 26番ピン(0x01 * 26)
         // PC26ピンのうちINENビット(Input Enable)を有効
-        *(PC_PINCFG as *mut u8) = 2;
+        unsafe {
+            *(PC_PINCFG as *mut u8) = 2;
+        }
 
         loop {
-            // PC26ピン(button1)が入力されていればLED ON
-            if *(PC_IN as *mut u32) & (1 << 26) != 0 {
-                *(PA_OUTCLR as *mut u32) = 1 << 15; // LED OFF(PA15)
-            } else {
-                *(PA_OUTSET as *mut u32) = 1 << 15;  // LED ON(PA15)
+            unsafe {
+                // PC26ピン(button1)が入力されていればLED ON
+                if *(PC_IN as *mut u32) & (1 << 26) != 0 {
+                    *(PA_OUTCLR as *mut u32) = 1 << 15; // LED OFF(PA15)
+                } else {
+                    *(PA_OUTSET as *mut u32) = 1 << 15;  // LED ON(PA15)
+                }
             }
         }
-    }
 }
